66c3c6bd087c15d1a1cb605a4d5d4c92c36d4eb9
gjx <326765926@qq.com>
2017-12-12 00:33:54 +0800

    毛玻璃实现

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var watch = require('gulp-watch');
 
 gulp.task('default', () => {
     return gulp.src('index.scss')
     .pipe(sass())
     .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('fun', () => {
+    return gulp.src('forFun/*.scss')
+    .pipe(watch())
+    .pipe(sass())
+    .pipe(gulp.dest('app/css/forFun'));
 });
