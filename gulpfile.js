985ddc940e3cb2cda445e2ecdd0c62c15ce37b8c
guojianxiang1 <guojianxiang1@henhaoji.com>
2018-05-15 12:10:38 +0800

    doc: 增加readme feat: size模块

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,12 @@ var sass = require('gulp-sass');
 
 gulp.task('default', () => {
     return gulp.src('index.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('app/css'));
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'));
 });
+
+gulp.task('test', () => {
+    return gulp.src('app/css/index.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('app/'))
+})
---
+++ readme.md
@@ -0,0 +1,8 @@
+### 希望能做到css模块化
+#### 想法是分为以下几个模块
+- size: 专门放距离的包括font-size,width,height,margin,padding
+- color: 常用颜色和项目使用的颜色
+- locate： 常用定位和项目使用的定位
+- dialog: 项目使用的弹窗
+- select: 项目使用的下拉框
+- btn: 项目使用的按钮
