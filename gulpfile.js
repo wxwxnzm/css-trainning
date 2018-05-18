d47351b5d9994ddc17730f5b16e29c0712caebe3
gjx <326765926@qq.com>
2018-05-18 11:08:20 +0800

    增加模块化例子：index.html vs-code 快捷键完善

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('default', () => {
-    return gulp.src('index.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('app/css'));
+gulp.task('turn', function() {
+    gulp.src(['app/scss/index.scss', 'app/scss/test.scss'])
+        .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(gulp.dest('app/css/'));
+})
+
+gulp.task('wt', () => {
+    gulp.watch('app/scss/*.scss',['turn']);
 });
 
-gulp.task('test', () => {
-    return gulp.src('app/css/index.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('app/'))
-})
