58931f6838166cf8cc0f1ead5082bf897e664997
guojianxiang <jianxiang.guo@xuebangsoft.me>
2018-05-30 14:34:25 +0800

    feat: less的支持： loop循环数组的实现

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var less = require('gulp-less');
 
 gulp.task('turn', function() {
     gulp.src(['app/scss/index.scss', 'app/scss/test.scss'])
@@ -11,3 +12,9 @@ gulp.task('wt', () => {
     gulp.watch('app/scss/*.scss',['turn']);
 });
 
+gulp.task('turn-less', function() {
+    // gulp.src(['app/scss/index.less', 'app/scss/test.less'])
+    gulp.src(['app/less/index.less'])
+        .pipe(less({outputStyle: 'expanded'}))
+        .pipe(gulp.dest('app/css/'));
+})
